a1f899e2f12ed56962713f06f868c5f2f1f0f036
Benjamin Morledge-Hampton <bmorledge-hampton19@my.whitworth.edu>
2016-10-25 15:03:40 -0700

    -Created Definition for Rectangle2D class

---
+++ Homework 4/Rectangle 2D/Rectangle2D.cpp
@@ -0,0 +1,70 @@
+#include "Rectangle2D.h"
+
+Rectangle2D::Rectangle2D()
+{
+}
+
+Rectangle2D::Rectangle2D(double x, double y, double width, double height)
+{
+}
+
+double Rectangle2D::getArea() const
+{
+	return 0.0;
+}
+
+double Rectangle2D::getPerimeter() const
+{
+	return 0.0;
+}
+
+bool Rectangle2D::contains(double x, double y) const
+{
+	return false;
+}
+
+bool Rectangle2D::contains(const Rectangle2D & r) const
+{
+	return false;
+}
+
+bool Rectangle2D::overlaps(const Rectangle2D & r) const
+{
+	return false;
+}
+
+double Rectangle2D::getX() const
+{
+	return 0.0;
+}
+
+void Rectangle2D::setX(double x)
+{
+}
+
+double Rectangle2D::getY() const
+{
+	return 0.0;
+}
+
+void Rectangle2D::setY(double y)
+{
+}
+
+double Rectangle2D::getWidth() const
+{
+	return 0.0;
+}
+
+void Rectangle2D::setWidth(double width)
+{
+}
+
+double Rectangle2D::getHeight() const
+{
+	return 0.0;
+}
+
+void Rectangle2D::setHeight(double height)
+{
+}
---
+++ Homework 4/Rectangle 2D/Rectangle2D.h
@@ -0,0 +1,37 @@
+#pragma once
+
+class Rectangle2D {
+
+private:
+
+	double x; // The x position of the rectangle.
+	double y; // The y position of the rectangle.
+	double width; // The width of the rectangle.
+	double height; // The height of the rectangle.
+
+public:
+
+	Rectangle2D(); // Default constructor.  Sets x and y as 0 and width and height as 1.
+	Rectangle2D(double x, double y, double width, double height);  // Values for width and height must be positive or they default to 1.
+	
+	double getArea() const; // Returns the rectangle's area.
+	double getPerimeter() const; // Returns the rectangle's perimeter.
+
+	bool contains(double x, double y) const; // Returns true if a point with the given x and y coordinates is within the rectangle.
+	bool contains(const Rectangle2D &r) const; // Returns true if the given rectangle lies completely within the class's rectangle.
+	bool overlaps(const Rectangle2D &r) const; // Returns true if the given rectangle overlaps the class's rectangle.
+
+	// Basic getters and setters follow:
+
+	double getX() const;
+	void setX(double x);
+
+	double getY() const;
+	void setY(double y);
+
+	double getWidth() const;
+	void setWidth(double width);
+
+	double getHeight() const;
+	void setHeight(double height);
+};
